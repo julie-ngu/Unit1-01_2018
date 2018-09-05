71ef78d1babdfacd1db8eb4636fdfdced9fd3336
Julie Nguyen <julie@ip-172-31-8-86.ca-central-1.compute.internal>
2018-09-05 18:44:28 +0000

    Hello World code

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// Created by: Julie Nguyen
+// Created on: Sept 2018
+// Created for ICS4U
+// This is the Hello World program
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //prints "Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
